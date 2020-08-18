502605be549330aa1a9b141c760edfa624cb6fc0
Kanda, Jiří <kanda@havit.cz>
2020-08-18 13:44:00 +0200

    Initial commit

---
+++ WebApplication7/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication7
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Azure.Storage.Blobs.BlobServiceClient serviceClient = new Azure.Storage.Blobs.BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=havittempstorage;AccountKey=/0000000000000000000000000000000000000000000000000000000000000000000000000000000000000==;EndpointSuffix=core.windows.net");
+			Azure.Storage.Blobs.BlobContainerClient containerClient = serviceClient.GetBlobContainerClient("any_container");
+			Azure.Storage.Blobs.BlobClient blobClient = containerClient.GetBlobClient("abc.txt");
+			blobClient.Exists(); // just run the method
+		}
+	}
+}
